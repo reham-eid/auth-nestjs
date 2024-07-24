b48630800a4f1d4194fcefe30616fe2e84ff3ff5
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-07-24 19:18:30 +0300

    Import UserMode in AuthModule and export authProvider, AuthService in UserModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { DatabaseModule } from "src/database/database.module";
 import { JwtModule } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { JwtModule } from "@nestjs/jwt";
       secret: process.env.SECRET_JWT,
       signOptions: { expiresIn: "60m" },
     }),
+    UserModule
   ],
   controllers: [AuthController],
   providers: [...authProvider, AuthService],
